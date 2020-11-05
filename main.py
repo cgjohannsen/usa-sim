b2952ed2331c7013ef35fe8894a6315786edc899
Chris Johannsen <chrisjohannsen8@gmail.com>
2020-11-05 13:56:57 -0600

    Adding basic file IO

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import argparse
+import csv
+
+from task import *
+
+def read_csv(filename):
+    task_set = []
+    with open(filename, newline='') as f:
+        reader = csv.reader(f)
+        for row in reader:
+            data = str(row).split(',')
+            task_set.append(Task(data[0], data[1], data[2], data[3]))
+
+    return task_set 
 
 def main():
     parser = argparse.ArgumentParser(description='Simulate uniprocessor algorithms.')
     parser.add_argument('file', help='csv file input with task set')
 
     args = parser.parse_args()
-    print(args)
+    task_set = read_csv(args.file)
+
+    for task in task_set:
+        print(task.id)
 
 if __name__ == "__main__":
     main()
